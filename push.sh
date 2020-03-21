1d93acf8370f49324b2c2bbfff51814d1575ccf3
Rajendra Singh <singh.raj1997@gmail.com>
2020-03-21 15:27:25 +0530

    updated

--- push.sh
+++ push.sh
@@ -12,5 +12,5 @@ parseArgv {*}$argv
 spawn git add $B; interact
 spawn git commit -m $A; interact
 spawn git push origin master; expect "Username:"; send "iamrajee\n"; expect "password:"; send "Raj@114232340\n"; interact
-exec clear
+# exec clear
 puts \033\[2J
