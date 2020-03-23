f4f652a3282137b840c60b5a83ed5a86a7c9f937
Rajendra Singh <singh.raj1997@gmail.com>
2020-03-23 13:04:12 +0530

    draft implementation of cartesian_position_motion
    one way for the moment

---
+++ src/moveit_task_constructor/include/moveit_task_constructor/subtasks/cartesian_position_motion.h
@@ -0,0 +1,52 @@
+// copyright Michael 'v4hn' Goerner @ 2017
+
+#pragma once
+
+#include <moveit_task_constructor/subtask.h>
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <geometry_msgs/msg/vector3_stamped.hpp>
+#include <geometry_msgs/msg/point_stamped.hpp>
+#include <moveit_msgs/msg/display_trajectory.hpp>
+
+namespace moveit::planning_interface {
+MOVEIT_CLASS_FORWARD(MoveGroupInterface);
+}
+
+namespace moveit::task_constructor::subtasks {
+
+class CartesianPositionMotion : public SubTask {
+public:
+	CartesianPositionMotion(std::string name);
+
+	virtual bool canCompute();
+
+	virtual bool compute();
+
+	void setGroup(std::string group);
+	void setLink(std::string link);
+
+	void setMinDistance(double distance);
+	void setMaxDistance(double distance);
+	void setMinMaxDistance(double min_distance, double max_distance);
+
+	void towards(geometry_msgs::msg::PointStamped goal);
+	void along(geometry_msgs::msg::Vector3Stamped direction);
+
+protected:
+	std::string group_;
+
+	std::string link_;
+
+	double min_distance_;
+	double max_distance_;
+
+	std::string mode_;
+	geometry_msgs::msg::PointStamped towards_;
+	geometry_msgs::msg::Vector3Stamped along_;
+
+	rclcpp::Publisher<moveit_msgs::msg::DisplayTrajectory>::SharedPtr pub; //ros::Publisher pub;
+};
+
+}
--- src/moveit_task_constructor/src/plan_pick.cpp
+++ src/moveit_task_constructor/src/plan_pick.cpp
@@ -4,6 +4,7 @@
 #include <moveit_task_constructor/subtasks/current_state.h>
 #include <moveit_task_constructor/subtasks/gripper.h>
 #include <moveit_task_constructor/subtasks/generate_grasp_pose.h>
+#include <moveit_task_constructor/subtasks/cartesian_position_motion.h>
 
 using namespace moveit::task_constructor;
 
@@ -29,14 +30,19 @@ int main(int argc, char** argv){
 		move->setPlanningTime(5.0);
 		t.addAfter(move);
 	}
-
+*/
 	{
 		auto move= std::make_shared<subtasks::CartesianPositionMotion>("proceed to grasp pose");
-		move->minMaxDistance(3.0, 10.0);
-		move->towards("object", 0.0, 0.0, 0.0);
+		move->setGroup("arm");
+		move->setLink("s_model_tool0");
+		move->setMinMaxDistance(.03, 0.1);
+
+		geometry_msgs::msg::PointStamped target;
+		target.header.frame_id= "object";
+		move->towards(target);
 		t.addAfter(move);
 	}
-*/
+
 	{
 		auto gengrasp= std::make_shared<subtasks::GenerateGraspPose>("generate grasp pose");
 		gengrasp->setEndEffector("s_model_tool0");
